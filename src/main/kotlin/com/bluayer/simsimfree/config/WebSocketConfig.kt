324278cc2e524a34f9fcbe56fef1580d40bcc89c
Jungwoo Song <37579681+bluayer@users.noreply.github.com>
2021-08-10 20:58:10 +0900

    feat: add configuration for websocket

---
+++ src/main/kotlin/com/bluayer/simsimfree/config/WebSocketConfig.kt
@@ -0,0 +1,22 @@
+package com.bluayer.simsimfree.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfig: WebSocketMessageBrokerConfigurer {
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/ws").withSockJS()
+    }
+
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        // Client To Server
+        registry.setApplicationDestinationPrefixes("/app")
+        // Server To Client
+        registry.enableSimpleBroker("/topic")
+    }
+}

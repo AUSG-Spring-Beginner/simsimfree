f07a317233b84cc9577f5cfaf2bcff01eeae9dcc
Jungwoo Song <37579681+bluayer@users.noreply.github.com>
2021-08-10 20:53:30 +0900

    feat: init commit

---
+++ src/main/kotlin/com/bluayer/simsimfree/SimsimfreeApplication.kt
@@ -0,0 +1,11 @@
+package com.bluayer.simsimfree
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimsimfreeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SimsimfreeApplication>(*args)
+}
---
+++ src/test/kotlin/com/bluayer/simsimfree/SimsimfreeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bluayer.simsimfree
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimsimfreeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

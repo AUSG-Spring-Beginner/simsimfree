ee485bddd9ab489dbeef2547a962329f12535710
Jungwoo Song <37579681+bluayer@users.noreply.github.com>
2021-08-11 10:02:31 +0900

    간단한 채팅 서비스 추가 (#1)
    * feat: add message type and class
    
    * feat: add controller for communication
    
    * feat: 간단하게 프론트 추가

---
+++ src/main/kotlin/com/bluayer/simsimfree/entity/ChatMessage.kt
@@ -0,0 +1,7 @@
+package com.bluayer.simsimfree.entity
+
+data class ChatMessage (
+    var type: MessageType,
+    var content: String?,
+    var sender: String
+)
---
+++ src/main/kotlin/com/bluayer/simsimfree/entity/MessageType.kt
@@ -0,0 +1,7 @@
+package com.bluayer.simsimfree.entity
+
+enum class MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
